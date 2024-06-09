37260f165df61a046ce5eb3255e86086542c4515
Michael <programmermike1@gmail.com>
2024-06-08 20:26:30 -0700

    run black

--- image_compressor.py
+++ image_compressor.py
@@ -5,7 +5,7 @@ import os
 
 
 def num_to_bits(n, num_bits):
-    assert(0 <= n < 2**num_bits)
+    assert 0 <= n < 2**num_bits
     return [1 if n & 2**i else 0 for i in range(num_bits - 1, -1, -1)]
 
 
@@ -54,7 +54,7 @@ def calculate_pairwise_encoding_with_LEB(bit_array, zero_len, one_len):
     i = 0
 
     while i < len(bit_array):
-        cur_max_len = (max_ones if cur_run_bit else max_zeros)
+        cur_max_len = max_ones if cur_run_bit else max_zeros
         if bit_array[i] == cur_run_bit and cur_run_len < cur_max_len:
             cur_run_len += 1
             i += 1
@@ -76,7 +76,7 @@ def calculate_bit_prefix_encoding(run_length_array, first_bit, num_len_bits):
         out.extend([cur_run_bit] + num_to_bits(r, num_len_bits))
         cur_run_bit = 1 - cur_run_bit
     return ceil((len(out) + 4) / 8), out
-    
+
 
 def encode_optimal_pairwise(bit_array):
     best_byte_size = inf
@@ -85,7 +85,9 @@ def encode_optimal_pairwise(bit_array):
     run_length_array, first_bit = bit_array_to_run_length_array(bit_array)
     for i in range(1, 13):
         for j in range(1, 13):
-            num_bytes, encoded = calculate_pairwise_encoding(run_length_array, first_bit, i, j)
+            num_bytes, encoded = calculate_pairwise_encoding(
+                run_length_array, first_bit, i, j
+            )
             if num_bytes < best_byte_size:
                 best_byte_size = num_bytes
                 best_num_bits = (i, j)
@@ -99,7 +101,9 @@ def encode_optimal_bit_prefix(bit_array):
     best_encoded = None
     run_length_array, first_bit = bit_array_to_run_length_array(bit_array)
     for i in range(1, 13):
-        num_bytes, encoded = calculate_bit_prefix_encoding(run_length_array, first_bit, i)
+        num_bytes, encoded = calculate_bit_prefix_encoding(
+            run_length_array, first_bit, i
+        )
         if num_bytes < best_byte_size:
             best_byte_size = num_bytes
             best_num_bits = i
@@ -113,20 +117,16 @@ def encode_optimal_naive(bit_array):
 
 
 def encode_optimal_monospace(bit_array):
-    max_run_len = 0
-    cur_run_len = 1
-    num_runs = 1
-    for i in range(1, len(bit_array)):
-        if bit_array[i] == bit_array[i - 1]:
-            cur_run_len += 1
-        else:
-            cur_run_len = 1
-            num_runs += 1
-        max_run_len = max(max_run_len, cur_run_len)
+    run_length_array, first_bit = bit_array_to_run_length_array(bit_array)
+    max_run_len = max(run_length_array)
     bits_per_run = ceil(log2(max_run_len + 1))
-    total_bits = bits_per_run * num_runs
+    begin_bit_cost = 1
+    bitsize_metadata_cost = 4
+    total_bits = (
+        bits_per_run * len(run_length_array) + begin_bit_cost + bitsize_metadata_cost
+    )
     num_bytes = ceil(total_bits / 8)
-    serialized_out = [] # unimplemented
+    serialized_out = []  # unimplemented
     return num_bytes, serialized_out
 
 
@@ -145,7 +145,7 @@ def test_whole_suite():
 
     algos = [(algo.__name__, algo) for algo in ENCODERS]
     # algo name => (sum, min, max)
-    summary_data = { algo_name: (0, inf, 0) for (algo_name, _) in algos}
+    summary_data = {algo_name: (0, inf, 0) for (algo_name, _) in algos}
     for img_name in json_pixels:
         print(f"Case {img_name}")
         pixel_str = json_pixels[img_name]
@@ -158,7 +158,7 @@ def test_whole_suite():
             cur_sum += num_bytes
             cur_min = min(cur_min, num_bytes)
             cur_max = max(cur_max, num_bytes)
-            summary_data[algo_name] = cur_sum, cur_min, cur_max 
+            summary_data[algo_name] = cur_sum, cur_min, cur_max
             print(f"  {algo_name} used {num_bytes} bytes")
 
     print("\n**SUMMARY**")
