31ee2b54c08569b68f61449ed4adfa9e29792764
Solomon Zitter <solomonzitter@gmail.com>
2024-06-08 18:49:40 -0700

    Improve naming

--- image_compressor.py
+++ image_compressor.py
@@ -20,7 +20,7 @@ def num_to_bits(n, num_bits):
     return [1 if n & 2**i else 0 for i in range(num_bits - 1, -1, -1)]
 
 
-def calculate_bisexual_encoding(bit_array, zero_len, one_len):
+def calculate_pairwise_encoding(bit_array, zero_len, one_len):
     max_zeros = 2**zero_len - 1
     max_ones = 2**one_len - 1
 
@@ -42,7 +42,7 @@ def calculate_bisexual_encoding(bit_array, zero_len, one_len):
     return ceil((len(out) + 4 + 4) / 8), out
 
 
-def calculate_bisexual_encoding_with_LEB(bit_array, zero_len, one_len):
+def calculate_pairwise_encoding_with_LEB(bit_array, zero_len, one_len):
     max_zeros = 2**zero_len - 1
     max_ones = 2**one_len - 1
 
@@ -62,7 +62,7 @@ def calculate_bisexual_encoding_with_LEB(bit_array, zero_len, one_len):
             # out.extend()
 
 
-def calculate_unisexual_encoding(bit_array, num_len_bits):
+def calculate_bit_prefix_encoding(bit_array, num_len_bits):
     max_len = 2**num_len_bits - 1
 
     out = []
@@ -84,43 +84,43 @@ def calculate_unisexual_encoding(bit_array, num_len_bits):
 
 if __name__ == "__main__":
     png_file = argv[1]
-    # print(calculate_bisexual_encoding(([0] * 3 + [1] * 7) * 2, 2, 3))
-    print(calculate_bisexual_encoding([0, 0, 0, 0, 1], 2, 2))
-    print(calculate_unisexual_encoding([0, 0, 0, 0, 1], 2))
+    # print(calculate_pairwise_encoding(([0] * 3 + [1] * 7) * 2, 2, 3))
+    print(calculate_pairwise_encoding([0, 0, 0, 0, 1], 2, 2))
+    print(calculate_bit_prefix_encoding([0, 0, 0, 0, 1], 2))
     bit_array = png_to_bit_array(png_file)
     # print(bit_array)
     # print(len(bit_array))
     # print(len(set(bit_array)))
-    best_bisexual_size = 512
-    best_bisexual_num_bits = (None, None)
-    best_bisexual_encoded = None
-    bisexual_sizes = []
+    best_pairwise_size = 512
+    best_pairwise_num_bits = (None, None)
+    best_pairwise_encoded = None
+    pairwise_sizes = []
     for i in range(1, 13):
         i_sizes = []
         for j in range(1, 13):
-            num_bytes, encoded = calculate_bisexual_encoding(bit_array, i, j)
-            if num_bytes < best_bisexual_size:
-                best_bisexual_size = num_bytes
-                best_bisexual_num_bits = (i, j)
-                best_bisexual_encoded = encoded
+            num_bytes, encoded = calculate_pairwise_encoding(bit_array, i, j)
+            if num_bytes < best_pairwise_size:
+                best_pairwise_size = num_bytes
+                best_pairwise_num_bits = (i, j)
+                best_pairwise_encoded = encoded
             i_sizes.append(str(len(encoded)))
-        bisexual_sizes.append(i_sizes)
-    # print("\n".join(map(lambda i_sizes: ','.join(i_sizes), bisexual_sizes)))
-    print(f"best bisexual size: {best_bisexual_size} (zero len {best_bisexual_num_bits[0]}, one len {best_bisexual_num_bits[1]})")
-    # print(best_bisexual_num_bits)
-    # print(best_bisexual_encoded)
-    # print(len([b for b in best_bisexual_encoded if b == 0]))
+        pairwise_sizes.append(i_sizes)
+    # print("\n".join(map(lambda i_sizes: ','.join(i_sizes), pairwise_sizes)))
+    print(f"best pairwise size: {best_pairwise_size} (zero len {best_pairwise_num_bits[0]}, one len {best_pairwise_num_bits[1]})")
+    # print(best_pairwise_num_bits)
+    # print(best_pairwise_encoded)
+    # print(len([b for b in best_pairwise_encoded if b == 0]))
 
-    best_unisexual_size = 512
-    best_unisexual_num_bits = None
-    best_unisexual_encoded = None
+    best_bit_prefix_size = 512
+    best_bit_prefix_num_bits = None
+    best_bit_prefix_encoded = None
     for i in range(1, 13):
-        num_bytes, encoded = calculate_unisexual_encoding(bit_array, i)
-        if num_bytes < best_unisexual_size:
-            best_unisexual_size = num_bytes
-            best_unisexual_num_bits = i
-            best_unisexual_encoded = encoded
-    print(f"best unisexual size: {best_unisexual_size} ({best_unisexual_num_bits} bit len)")
-    # print(best_unisexual_num_bits)
+        num_bytes, encoded = calculate_bit_prefix_encoding(bit_array, i)
+        if num_bytes < best_bit_prefix_size:
+            best_bit_prefix_size = num_bytes
+            best_bit_prefix_num_bits = i
+            best_bit_prefix_encoded = encoded
+    print(f"best bit prefix size: {best_bit_prefix_size} ({best_bit_prefix_num_bits} bit len)")
+    # print(best_bit_prefix_num_bits)
 
 
